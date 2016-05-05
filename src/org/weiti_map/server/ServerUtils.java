fd9d1f5dbc66492835805c0a3229f304f083718e
Mozart <kioladusa@gmail.com>
2016-05-05 22:03:49 +0200

    master_push

--- src/org/weiti_map/server/ServerUtils.java
+++ src/org/weiti_map/server/ServerUtils.java
@@ -1,6 +1,7 @@
 package org.weiti_map.server;
 
 class ServerUtils {
-	final static String DEFAULT_KEYSTORE_NAME = "wmapdefaultkeystore_";
-	final static String EMAIL_ADDRESS = "jakubkoladadev@gmail.com";
+//	final static String DEFAULT_KEYSTORE_NAME = "wmapdefaultkeystore_";
+	final static String EMAIL_ADDRESS = "<HANDSHAKE/jakubkoladadev@gmail.com>";
+	final static String GET_GROUP = "<GET_GROUP/";
 }
